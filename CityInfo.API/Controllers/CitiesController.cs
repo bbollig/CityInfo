9d8a1b7ddda2869dc9f9c7dc14030b53f525960e
Brian Bollig <Brian.Bollig@granicus.com>
2019-08-02 10:57:25 -0600

    Added a GetCity operation plus some comments to help me remember some concepts

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -9,10 +9,21 @@ namespace CityInfo.API.Controllers
     [Route("api/cities")]
     public class CitiesController : Controller
     {
+        //[HttpGet("api/cities")]<!--if decorating each operation with a fully qualified HTTP attribute, 
+        //the route would look like this but we are using 'api/cities' as part of the route template for our controller, seen above at the controller level
         [HttpGet()]
         public JsonResult GetCities()
         {
             return new JsonResult(CitiesDataStore.Current.Cities);
+        }
+
+        //[HttpGet("api/cities/{id}")] <--This is what he fully qualified route would look like
+        [HttpGet("{id}")]//<--To work with parameters with http attributes, we use the curly brackets
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(
+                CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id)
+                );
         }
     }
 }
