c02c8a72b36f63bcf087138f353d384bb2f8c660
Brian Bollig <Brian.Bollig@granicus.com>
2019-08-02 10:31:42 -0600

    Declaring Route at the Controller level, instead of in each HTTP attribute. Removed dummy data created in the call in favor of calling the datastore.

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -6,16 +6,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
 {
+    [Route("api/cities")]
     public class CitiesController : Controller
     {
-        [HttpGet("api/cities")]
+        [HttpGet()]
         public JsonResult GetCities()
         {
-            return new JsonResult(new List<object>()
-            {
-                new { id=1, Name="New York City" },
-                new { id=2, Name="Antwerp" }
-            });
+            return new JsonResult(CitiesDataStore.Current.Cities);
         }
     }
 }
