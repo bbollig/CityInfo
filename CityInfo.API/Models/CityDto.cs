e6651c72432887fbca622ad03edc63f1076ea9ab
Brian Bollig <Brian.Bollig@granicus.com>
2019-08-02 12:18:30 -0600

    Added PointOfInterestDto class. Added List<PointOfInterest> property to CityDto and updated property NumberOfPointsOfInterest to return a count of this.

--- CityInfo.API/Models/CityDto.cs
+++ CityInfo.API/Models/CityDto.cs
@@ -13,6 +13,14 @@ namespace CityInfo.API.Models
 
         public string Description { get; set; }
 
-        public int NumberOfPointsOfInterest { get; set; }
+        public int NumberOfPointsOfInterest
+        {
+            get
+            {
+                return PointsOfInterest.Count;
+            }
+        }
+
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; } = new List<PointOfInterestDto>();
     }
 }
