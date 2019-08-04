cae2838d081e637ab9e1f07f9fb9da5f987da126
Brian Bollig <Brian.Bollig@granicus.com>
2019-08-04 12:57:03 -0600

    Added IMailService to the class

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,4 +1,5 @@
 ﻿using CityInfo.API.Models;
+using CityInfo.API.Services;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -11,11 +12,13 @@ namespace CityInfo.API.Controllers
     public class PointsOfInterestController : Controller
     {
         private ILogger<PointsOfInterestController> _logger;
+        private IMailService _mailService;
 
         //using constructor injection, we can request Services from the services IoC control
-        public PointsOfInterestController(ILogger<PointsOfInterestController> logger)
+        public PointsOfInterestController(ILogger<PointsOfInterestController> logger, IMailService mailService)
         {
             _logger = logger;
+            _mailService = mailService;
         }
 
         [HttpGet("{cityid}/pointsofinterest")]
@@ -209,6 +212,9 @@ namespace CityInfo.API.Controllers
             }
 
             city.PointsOfInterest.Remove(pointOfInterestFromStore);
+
+            _mailService.Send("Point of Interest Deleted.", $"Point of Interest {pointOfInterestFromStore.Name} " +
+                $"with ID {pointOfInterestFromStore.Id} was deleted.");
 
             return NoContent();
         }
