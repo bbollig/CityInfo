ab972ba3966c7c19ca67960269e5d574faced032
Brian Bollig <Brian.Bollig@granicus.com>
2019-08-04 12:53:12 -0600

    Created separate "Mail Services" for development vs. production environments.

---
+++ CityInfo.API/Services/CloudMailService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static System.Diagnostics.Debug;
+
+
+namespace CityInfo.API.Services
+{
+    public class CloudMailService : IMailService
+    {
+        private string _maitTo = Startup.Configuration["mailSettings:mailToAddress"];
+        private string _mailFrom = Startup.Configuration["mailSettings:mailFromAddress"];
+
+        public void Send(string subject, string message)
+        {
+            //send mail - output to debug window
+            WriteLine($"Mail from {_mailFrom} to {_maitTo} with CloudMailService.");
+            WriteLine($"Subject: {subject}");
+            WriteLine($"Message: {message}");
+        }
+
+    }
+}
---
+++ CityInfo.API/Services/LocalMailService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static System.Diagnostics.Debug;
+
+namespace CityInfo.API.Services
+{
+    public class LocalMailService : IMailService
+    {
+        private string _maitTo = Startup.Configuration["mailSettings:mailToAddress"];
+        private string _mailFrom = Startup.Configuration["mailSettings:mailFromAddress"];
+
+        public void Send(string subject, string message)
+        {
+            //send mail - output to debug window
+            WriteLine($"Mail from {_mailFrom} to {_maitTo} with LocalMailService.");
+            WriteLine($"Subject: {subject}");
+            WriteLine($"Message: {message}");
+        }
+    }
+}
