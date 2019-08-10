2bb2fcf44d953f3164a2471c3fe20e72cef17385
Brian Bollig <Brian.Bollig@granicus.com>
2019-08-09 22:25:15 -0600

    Then I had to create a class that inherits from AutoMapper.Profile to house the Entity to Model mapping relationships

---
+++ CityInfo.API/Profiles/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Entities.City, Models.CityWithoutPointsOfInterestDto>();
+            CreateMap<Entities.City, Models.CityDto>();
+            CreateMap<Entities.PointOfInterest, Models.PointOfInterestDto>();
+        }
+    }
+}
